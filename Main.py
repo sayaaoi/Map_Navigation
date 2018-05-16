796e4826ed8cf540fabc5d19f52606e24ec815c4
Amber Wu <yuejun.wu1115@gmail.com>
2018-05-16 13:24:35 -0500

    edit user interface for node attributes

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 from Navigation import *
 from pathlib import Path
 
-
 # colored console output
 red_col = '\033[91m'
 blue_col = '\033[94m'
@@ -16,23 +15,19 @@ while True:
     prompt = input("Do you want to see an example of a map navigation or create one on your own? \n1. Example  \n2. Create a new one \n3. Quit\n")
     if prompt == "1":
         site_name = "Xcaret Amusement Park"
-        node_file, edge_file = "data/node_list.csv", "data/edge_list.csv"
+        node_file, edge_file = "data/node_list_new.csv", "data/edge_list_new.csv"
         break
     elif prompt == "2":
         site_name = input("What's the name of the place your map is designed for? ")
-        trial_times = 0
         while True:
             node_file_name = input("Please indicate the file path of node list: ")
-            trial_times += 1
             node_file = Path(node_file_name)
             try:
                 node_file.resolve(strict = True)
             except FileNotFoundError:
                 print("File path doesn't exit. Please try again.\n")
                 continue
-            if trial_times == 2:
-                print("You've tried too many times.")
-            elif not node_file_name[-4:] == ".csv":
+            if not node_file_name[-4:] == ".csv":
                 print("Node file must be in .csv format. Please try again. \n")
             else:
                 break
@@ -55,12 +50,35 @@ while True:
 
 
 def user_interface():
-    map = Map(node_file, edge_file)
+    map_img = Map(node_file, edge_file, False)
 
     print("""=========================== Welcome to %s ============================
                     -------- Attraction names and ID numbers -------- \n""" % site_name)
     # print all attractions alphabetically
-    map.print_all_attractions()
+    print(map_img.print_all_attractions())
+    while True:
+        node_attr_q = input("Would you like to know some information about a specific attraction? \n1.Yes\n2.No\n")
+        if node_attr_q == "1":
+            while True:
+                try:
+                    which_node = eval(input("Please type in the attraction number: "))
+                except NameError as err:
+                    print(err, "Please try again!")
+                    continue
+                if which_node not in map_img.get_map().nodes:
+                    print("Wrong attraction number. Please type again.")
+                else:
+                    map_img.get_nodes_attributes(which_node)
+                    break
+                    # while True:
+                    #     addition = input("Would you like to know information about other nodes? \n1.Yes\n2.No\n")
+                    #     if addition == "2":
+
+            break
+        elif node_attr_q == "2":
+            exit()
+        else:
+            print("Invalid input. Please try again!")
 
 if __name__ == "__main__":
     user_interface()
