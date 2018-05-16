15608661a9f851c38168076ad1f068dc73ae55da
Amber Wu <yuejun.wu1115@gmail.com>
2018-05-16 09:46:36 -0500

    make draw_route a function outside of Map class

--- Navigation.py
+++ Navigation.py
@@ -348,49 +348,52 @@ class Map:
         plt.axis("off")
         return plt.show()
 
-    def draw_route(self, src: int, dest: int, graph_name: str):
-        """
-        Visualize shortest path between nodes.
 
-        :param src: starting node number
-        :param dest: ending node number
-        :return: map with highlighted shortest path
-        """
-        #plt.subplot(111)
-        fig = plt.figure(figsize=(15, 10))
-        temp = self.shortest_path(src, dest, weight='distance')
-        # The positions of each node are stored in a dictionary
-        node_pos = nx.get_node_attributes(self._map, 'pos')
-        # Water edges
-        edge_water = [(data[0], data[1]) for data in list(self._map.edges.data('type')) if data[2] == "water"]
-        # extract node number
-        path = [loc[0] for loc in temp]
+def draw_route(src: int, dest: int, graph_name: str, graph):
+    """
+    Visualize shortest path between nodes.
 
-        # create a list of edges in the shortest path using the zip command and store it in red edges
-        red_edges = list(zip(path, path[1:]))
-        # if the node is in the shortest path, set it to red, else set it to white color
-        node_col = ['white' if node not in path else 'red' for node in self._map.nodes]
-        edge_col = ['black' if edge not in red_edges else 'red' for edge in self._map.edges]
-        edge_width = [1 if edge not in red_edges else 4 for edge in self._map.edges]
-        edge_label_text = {}
-        for edge in self._map.edges:
-            if edge in edge_water:
-                edge_label_text[edge] = "water"
-            else:
-                edge_label_text[edge] = ""
-        # draw the nodes
-        nx.draw_networkx(self._map, node_pos, node_color=node_col, node_size=1000, font_size=13)
-        nx.draw_networkx_nodes(self._map, node_pos, edgecolors='black', node_color=node_col, node_size=1000)
-        # draw the edges
-        nx.draw_networkx_edges(self._map, node_pos, edge_color=edge_col, width=edge_width)
-        # Draw the edge labels
-        nx.draw_networkx_edge_labels(self._map, node_pos, edge_labels=edge_label_text, font_size=8)
-        #fig.set_facecolor('#96f97b')
-        plt.axis("off")
+    :param src: starting node number
+    :param dest: ending node number
+    :param graph_name: name of the map
+    :param graph: graph object, could be any Graph(nx.Graph(), nx.DiGraph(), etc.)
+    :return: map with highlighted shortest path
+    """
+    #plt.subplot(111)
+    fig = plt.figure(figsize=(15, 10))
+    fig.suptitle(graph_name, fontsize=30)
+    # get nodes numbers in shortest path
+    path = nx.dijkstra_path(graph, src, dest, weight="distance")
+    # The positions of each node are stored in a dictionary
+    node_pos = nx.get_node_attributes(graph, 'pos')
+    # Water edges
+    edge_water = [(data[0], data[1]) for data in list(graph.edges.data('type')) if data[2] == "water"]
 
-        # plt.subplot(112)
-        # self.draw_map(graph_name)
-        return plt.show()
+    # create a list of edges in the shortest path using the zip command and store it in red edges
+    red_edges = list(zip(path, path[1:]))
+    # if the node is in the shortest path, set it to red, else set it to white color
+    node_col = ['white' if node not in path else 'red' for node in graph.nodes]
+    edge_col = ['black' if edge not in red_edges else 'red' for edge in graph.edges]
+    edge_width = [1 if edge not in red_edges else 4 for edge in graph.edges]
+    edge_label_text = {}
+    for edge in graph.edges:
+        if edge in edge_water:
+            edge_label_text[edge] = "water"
+        else:
+            edge_label_text[edge] = ""
+    # draw the nodes
+    nx.draw_networkx(graph, node_pos, node_color=node_col, node_size=1000, font_size=13)
+    nx.draw_networkx_nodes(graph, node_pos, edgecolors='black', node_color=node_col, node_size=1000)
+    # draw the edges
+    nx.draw_networkx_edges(graph, node_pos, edge_color=edge_col, width=edge_width)
+    # Draw the edge labels
+    nx.draw_networkx_edge_labels(graph, node_pos, edge_labels=edge_label_text, font_size=8)
+    #fig.set_facecolor('#96f97b')
+    plt.axis("off")
+
+    # plt.subplot(112)
+    # self.draw_map(graph_name)
+    return plt.show()
 
 
 if __name__ == "__main__":
@@ -404,8 +407,8 @@ if __name__ == "__main__":
 #gs.get_nodes_attributes(9)
 #print(gs.find_nearest_bathroom(12))
     #print(gs.go_through_all_nodes())
-    #gs.draw_route(39,36,'Sample')
-    gs.draw_map("sample")
+    draw_route(39,36,'Sample',gs.get_map())
+    #gs.draw_map("sample")
     #print(gs.print_all_attractions())
 #gs.shortest_path(9,20)
 # gs.print_all_attractions()
